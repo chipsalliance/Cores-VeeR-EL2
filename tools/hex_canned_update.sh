f1e1b27cdff3231fb63b0ecc0abe3c2b8ce175c7
Michal Czyz <mczyz@antmicro.com>
2023-03-21 16:43:43 +0100

    Update canned hex files after rebranding

--- tools/hex_canned_update.sh
+++ tools/hex_canned_update.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Expected usage:
+# Inside `cores-veer-el2` directory run:
+# bash tools/hex_canned_update.sh
 
 export RV_ROOT=$(pwd)
 echo "RV_ROOT =" $RV_ROOT
@@ -9,13 +12,16 @@ HEX_DIR=testbench/hex
 
 for test in ${TESTS[@]}; do
     $MAKE_CMD clean > /dev/null
-    $MAKE_CMD program.hex TEST=hello_world > /dev/null
+    $MAKE_CMD program.hex TEST=$test > /dev/null
     echo "TEST = " $test
     if [ -f "program.hex" ]; then
         echo "Copying $test:program.hex to ["$HEX_DIR/$test.hex"]"
         cp program.hex $HEX_DIR/$test.hex
     else
         echo "program.hex not found. Possible build error."
+        exit 1
     fi
 done
+
+exit 0
 

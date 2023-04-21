335df413715133ec449a8e35396b246f189acafd
Rafal Kolucki <rkolucki@antmicro.com>
2023-04-21 17:11:20 +0200

    Add mailbox monitor and cycle limit coroutines
    Signed-off-by: Rafal Kolucki <rkolucki@antmicro.com>

--- verification/common.py
+++ verification/common.py
@@ -1,6 +1,7 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import RisingEdge, Edge
+from cocotb.result import TestSuccess, TestFailure
 
 
 class Harness(object):
@@ -15,12 +16,31 @@ class Harness(object):
         clock_sig = cocotb.start_soon(clock.start())
         return clock_sig
 
-    @cocotb.coroutine
-    async def reset(self):
-        self.dut.rst_l.value = 1
-        await ClockCycles(self.dut.core_clk, 2)
-        self.dut.rst_l.value = 0
-        await ClockCycles(self.dut.core_clk, 2)
-        self.dut.rst_l.value = 1
-        await ClockCycles(self.dut.core_clk, 2)
+    def finish(self, exec_result):
+        self.clk_gen.kill()
 
+        self.dut._log.info("minstret = %0d" % (self.dut.rvtop.veer.dec.tlu.minstretl.value))
+        self.dut._log.info("mcycle = %0d" % (self.dut.rvtop.veer.dec.tlu.mcyclel.value))
+
+        if exec_result == True:
+            raise TestSuccess("Program execution passed")
+        if exec_result == False:
+            raise TestFailure("Program execution failed")
+
+    @cocotb.coroutine
+    async def mailbox_monitor(self, asciioutput=None):
+        while True:
+            await RisingEdge(self.dut.mailbox_write)
+            if self.dut.mailbox_data_val.value and asciioutput is not None:
+                asciioutput(chr(self.dut.WriteData.value & 0xff))
+            if (self.dut.WriteData.value & 0xff) == 0xff:
+                self.dut._log.info("TEST_PASSED")
+                return True
+            if (self.dut.WriteData.value & 0xff) == 0x01:
+                self.dut._log.info("TEST_FAILED")
+                return False
+
+    @cocotb.coroutine
+    async def cycle_monitor(self):
+        while await Edge(self.dut.cycleCnt):
+            assert self.dut.cycleCnt.value.integer < self.dut.MAX_CYCLES.value.integer
--- verification/test_base.py
+++ verification/test_base.py
@@ -1,5 +1,4 @@
 import cocotb
-from cocotb.triggers import ClockCycles
 from .common import Harness
 
 
@@ -7,8 +6,18 @@ from .common import Harness
 async def test_basic(dut):
     harness = Harness(dut, 100)
 
-    await harness.reset()
+    # Clock starts when instancing harness.
+    # Reset is being held by testbench for 5 clock cycles
 
-    await ClockCycles(harness.dut.core_clk, 100)
+    print("JTAG ID: ", hex(dut.jtag_id.value))
 
-    harness.clk_gen.kill()
+    # Wait until program reports success or failure via mailbox
+    # or timeouts (cycle limit).
+    # You can pass file object `write()`` function or compatible
+    # via `asciioutput` optional argument.
+    cocotb.start_soon(harness.cycle_monitor())
+    exec_result = await harness.mailbox_monitor()
+
+    # Simulation is paused after raising test result in cocotb,
+    # so we read cycle counter here.
+    harness.finish(exec_result)

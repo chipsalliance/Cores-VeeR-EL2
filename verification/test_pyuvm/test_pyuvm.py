01ba37ce6281fc47661f1d33d67774e261882f01
Michal Czyz <mczyz@antmicro.com>
2023-06-05 11:02:03 +0200

    Implement minimal PyUVM test environment

---
+++ verification/test_pyuvm/test_pyuvm.py
@@ -0,0 +1,27 @@
+import pytest
+import os
+import subprocess
+
+
+class TestPyUVM():
+
+    @pytest.mark.parametrize("UVM_TEST", ["test_irq.test_irq"])
+    def test_pyuvm(self, UVM_TEST):
+
+        os.environ["UVM_TEST"] = UVM_TEST
+        py_command = []
+        py_command += [
+            "make all",
+        ]
+        py_command = " ".join(py_command)
+
+        print(f"\n----- PyTest -----")
+        print(f":: py_command >> {py_command}")
+        p = subprocess.run(py_command, shell=True,
+                           executable="/bin/bash", bufsize=0)
+        print(f"\n------------------")
+
+        print(f"----- Subprocess Summary -----")
+        print(f"p.check_returncode")
+        p.check_returncode()
+        print(f"------------------------------")

de87df8c51f5017fe7445ea0e081b0da29252e84
Maciej Kurc <mkurc@antmicro.com>
2023-09-19 15:32:53 +0200

    Adjust GitHub actions CI for microarchitectural tests
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- .github/scripts/convert_coverage_data.sh
+++ .github/scripts/convert_coverage_data.sh
@@ -4,46 +4,41 @@ SELF_DIR="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
 . ${SELF_DIR}/common.inc.sh
 
 convert_coverage_data(){
-    # This function uses verilator_coverage module to convert a coverage .dat file:
-    #   ${DAT_DIR}/coverage.dat
-    # into an .info file:
-    #   ${RESULTS_DIR}/${FILE_PREFIX}_${COVERAGE}.info
+    # This function uses verilator_coverage module to convert a coverage .dat
+    # file(s) into an .info file(s) for further processing.
     # Args:
-    #   COVERAGE : type of coverage
-    #   DAT_DIR: path to dir containing coverage.dat file
-    #   RESULTS_DIR: path to dir, where .info file will be placed
-    #   FILE_PREFIX: prefix used in the name of coverage_.info
-    check_args_count $# 4
-    COVERAGE=$1
-    DAT_DIR=$2
-    RESULTS_DIR=$3
-    FILE_PREFIX=$4
-    echo -e "${COLOR_WHITE}======= convert_coverage_data =======${COLOR_CLEAR}"
-    echo -e "${COLOR_WHITE}COVERAGE    = ${COVERAGE}"
-    echo -e "${COLOR_WHITE}DAT_DIR     = ${DAT_DIR}"
-    echo -e "${COLOR_WHITE}RESULTS_DIR = ${RESULTS_DIR}"
-    echo -e "${COLOR_WHITE}FILE_PREFIX = ${FILE_PREFIX}"
-    echo -e "${COLOR_WHITE}========== ${COVERAGE} coverage ==========${COLOR_CLEAR}"
+    #   DAT_DIR: path to dir containing coverage.dat file(s)
+    DAT_DIR="${1:-results_verification}"
+    echo -e "${COLOR_WHITE}======= Parse arguments =======${COLOR_CLEAR}"
+    echo -e "${COLOR_WHITE}DAT_DIR = ${DAT_DIR}"
+    echo -e "${COLOR_WHITE}===============================${COLOR_CLEAR}"
 
     # Function body
-    if ! [ -f "${DAT_DIR}/coverage.dat" ]; then
-        echo -e "${COLOR_WHITE}coverage.dat not found in dir=${DAT_DIR} ${COLOR_RED}FAIL${COLOR_CLEAR}"
+    FILES=`find ${DAT_DIR} -name "coverage*.dat"`
+    if [ -z "$FILES" ]; then
+        echo -e "${COLOR_RED}ERROR: No coverage data files were found${COLOR_CLEAR}"
+        echo -e "${COLOR_RED}ERROR: Searched directory: `realpath ${DAT_DIR}`${COLOR_CLEAR}"
+        echo -e "${COLOR_RED}ERROR: convert_coverage_data ended with errors${COLOR_CLEAR}"
         exit -1
     else
-        mkdir -p ${RESULTS_DIR}
-        cp ${DAT_DIR}/coverage.dat ${RESULTS_DIR}/${FILE_PREFIX}_${COVERAGE}.dat
-        verilator_coverage --write-info ${RESULTS_DIR}/${FILE_PREFIX}_${COVERAGE}.info ${RESULTS_DIR}/${FILE_PREFIX}_${COVERAGE}.dat
-        echo -e "${COLOR_WHITE}Conversion: ${DAT_DIR}/coverage.dat -> ${RESULTS_DIR}/${FILE_PREFIX}_${COVERAGE}.info ${COLOR_GREEN}SUCCEEDED${COLOR_CLEAR}"
+        for dat_file in ${FILES}; do
+            info_file=`basename -s .dat ${dat_file}`.info
+            info_realpath=`realpath \`dirname ${dat_file}\``
+            info_file=${info_realpath}/${info_file}
+            verilator_coverage --write-info ${info_file} ${dat_file}
+            echo -e "${COLOR_WHITE}Conversion: ${dat_file} -> ${info_file} ${COLOR_GREEN}SUCCEEDED${COLOR_CLEAR}"
+        done
     fi
 }
 
 # Example usage
-# RESULTS_DIR="results"
-# COVERAGE="branch"
-# DAT_DIR="."
-# FILE_PREFIX="coverage_test"
+# DAT_DIR="results_verification"
 #
-# convert_coverage_data $COVERAGE $DAT_DIR $RESULTS_DIR $FILE_PREFIX
+# convert_coverage_data $DAT_DIR
 
-check_args_count $# 4
+echo -e "${COLOR_WHITE}========== convert_coverage_data ==============${COLOR_CLEAR}"
+
 convert_coverage_data "$@"
+
+echo -e "${COLOR_WHITE}convert_coverage_data ${COLOR_GREEN}SUCCEEDED${COLOR_CLEAR}"
+echo -e "${COLOR_WHITE}========== convert_coverage_data ==============${COLOR_CLEAR}"

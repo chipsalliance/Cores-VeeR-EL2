4c55549b8cefa878c61de3264ebef6eb4edf3238
Aleksander Kiryk <akiryk@antmicro.com>
2023-09-27 18:56:06 +0200

    Add TL tests

---
+++ verification/block/dec_tl/test_dec_tl.py
@@ -0,0 +1,28 @@
+# Copyright (c) 2023 Antmicro <www.antmicro.com>
+# SPDX-License-Identifier: Apache-2.0
+import random
+
+import pyuvm
+from cocotb.triggers import ClockCycles
+from pyuvm import *
+from testbench import TlSequence, BaseTest
+
+# =============================================================================
+
+
+@pyuvm.test()
+class TestTriggerLogic(BaseTest):
+    def end_of_elaboration_phase(self):
+        super().end_of_elaboration_phase()
+        self.seq = TlSequence("stimulus")
+
+    async def run_phase(self):
+        self.raise_objection()
+
+        # Run the actual test
+        await self.run()
+
+        self.drop_objection()
+
+    async def run(self):
+        await self.seq.start(self.env.tl_seqr)

aeee423a15b5bf3937c6583a9cad56e579f79dce
Ryszard Rozak <rrozak@antmicro.com>
2024-06-13 08:50:58 +0200

    Use --extract option to exclude every file outside design dir
    Internal-tag: [#60579]
    Signed-off-by: Ryszard Rozak <rrozak@antmicro.com>

--- .github/scripts/gen_coverage_reports.sh
+++ .github/scripts/gen_coverage_reports.sh
@@ -16,10 +16,7 @@ generate_coverage_reports(){
     echo -e "${COLOR_WHITE}GENHTML_OPTS    = ${GENHTML_OPTS}${COLOR_CLEAR}"
 
     for info_file in `find . -name '*.info'`; do
-        for dir in verification testbench; do
-            lcov -r ${info_file} \*${dir}\* -o ${info_file}
-        done
-        lcov -r ${info_file} pic_map_auto.h -o ${info_file}
+        lcov --extract ${info_file} \*design\* -o ${info_file}
     done
 
     for COVERAGE in branch toggle all functional; do

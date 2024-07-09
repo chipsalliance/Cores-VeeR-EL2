b05551d1690c2f8472850fcb4046edaefc8b2924
Filip Kokosinski <fkokosinski@antmicro.com>
2024-07-09 21:52:47 +0200

    scripts: calculate hashes for RISCV-DV user extension files
    Signed-off-by: Filip Kokosinski <fkokosinski@antmicro.com>

--- .github/scripts/get_code_hash.sh
+++ .github/scripts/get_code_hash.sh
@@ -8,5 +8,7 @@ HASHES+=($(sha256sum tools/riscv-dv/code_fixup.py | cut -d\  -f1))
 HASHES+=($(sha256sum tools/riscv-dv/testlist.yaml | cut -d\  -f1))
 HASHES+=($(sha256sum tools/riscv-dv/riscv_core_setting.sv | cut -d\  -f1))
 HASHES+=($(sha256sum tools/riscv-dv/Makefile | cut -d\  -f1))
+HASHES+=($(sha256sum tools/riscv-dv/user_extension.svh | cut -d\  -f1))
+HASHES+=($(sha256sum tools/riscv-dv/veer_directed_instr_lib.sv | cut -d\  -f1))
 
 echo ${HASHES[@]} | sha256sum | cut -d\  -f1

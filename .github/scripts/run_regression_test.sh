98833db22a144a815e1a8567d3ce6892886894ab
Wojciech Sipak <wsipak@antmicro.com>
2024-07-22 15:22:26 +0200

    Run regression tests with both u-moade enabled and disabled

--- .github/scripts/run_regression_test.sh
+++ .github/scripts/run_regression_test.sh
@@ -10,18 +10,25 @@ run_regression_test(){
     # BUS -
     # NAME -
     # COVERAGE -
-    check_args_count $# 4
+    # USER_MODE - '1' for user mode, '0' for without user mode
+    check_args_count $# 5
     RESULTS_DIR=$1
     BUS=$2
     NAME=$3
     COVERAGE=$4
+    USER_MODE=$5
     echo -e "${COLOR_WHITE}========== running test '${NAME}' =========${COLOR_CLEAR}"
     echo -e "${COLOR_WHITE} RESULTS_DIR = ${RESULTS_DIR}${COLOR_CLEAR}"
     echo -e "${COLOR_WHITE} SYSTEM BUS  = ${BUS}${COLOR_CLEAR}"
     echo -e "${COLOR_WHITE} NAME        = ${NAME}${COLOR_CLEAR}"
     echo -e "${COLOR_WHITE} COVERAGE    = ${COVERAGE}${COLOR_CLEAR}"
+    echo -e "${COLOR_WHITE} USER_MODE   = ${USER_MODE}${COLOR_CLEAR}"
 
-    COMMON_PARAMS="-set=user_mode=1 -set=smepmp=1"
+    COMMON_PARAMS=""
+
+    if [[ "${USER_MODE}" == "1" ]]; then
+        COMMON_PARAMS="-set=user_mode=1 -set=smepmp=1 ${COMMON_PARAMS}"
+    fi
 
     if [[ "${BUS}" == "axi" ]]; then
         PARAMS="-set build_axi4 ${COMMON_PARAMS}"
@@ -35,9 +42,9 @@ run_regression_test(){
     echo -e "${COLOR_WHITE} CONF PARAMS = ${PARAMS}${COLOR_CLEAR}"
 
     mkdir -p ${RESULTS_DIR}
-    LOG="${RESULTS_DIR}/test_${NAME}_${COVERAGE}.log"
+    LOG="${RESULTS_DIR}/test_${NAME}_${COVERAGE}_${USER_MODE}.log"
     touch ${LOG}
-    DIR="run_${NAME}_${COVERAGE}"
+    DIR="run_${NAME}_${COVERAGE}_${USER_MODE}"
 
     # Run the test
     mkdir -p ${DIR}
@@ -46,7 +53,7 @@ run_regression_test(){
         echo -e "${COLOR_WHITE}Test '${NAME}' ${COLOR_RED}FAILED${COLOR_CLEAR}"
         exit 1
     else
-        mv ${DIR}/coverage.dat ${RESULTS_DIR}/
+        mv ${DIR}/coverage.dat ${RESULTS_DIR}/coverage_${USER_MODE}.dat
         echo -e "${COLOR_WHITE}Test '${NAME}' ${COLOR_GREEN}SUCCEEDED${COLOR_CLEAR}"
         exit 0
     fi
@@ -60,7 +67,5 @@ run_regression_test(){
 # run_regression_test.sh $RESULTS_DIR $BUS $NAME $COVERAGE
 
 check_args_count $# 4
-run_regression_test "$@"
-
-
-
+run_regression_test "$@" "1"
+run_regression_test "$@" "0"

0dcd4f914eb48442a397e76ecca807caf19fddf2
Krzysztof Obłonczek <koblonczek@antmicro.com>
2024-08-20 15:44:18 +0200

    Remove substituting names in makefiles, point default paths to current directory, remove old header files
    Internal-tag: [#43412]
    Signed-off-by: Krzysztof Obłonczek <koblonczek@antmicro.com>

--- tools/prefix_macros.sh
+++ tools/prefix_macros.sh
@@ -3,23 +3,24 @@
 # Prefix that will be added to all required macro/struct/module names
 PREFIX="veer0_"
 # Path to directory where common_defines.vh, el2_param.vh, el2_pdef.vh and pd_defines.vh reside
-DEFINES_PATH="../verification/block/snapshots/default"
+DEFINES_PATH="."
 # Path to directory hierarchy where RTL sources reside
-DESIGN_DIRS="../design ../verification ../testbench"
+DESIGN_DIR="."
 
 COMMON_DEFINES="$DEFINES_PATH/common_defines.vh"
 EL2_PARAM="$DEFINES_PATH/el2_param.vh"
 EL2_PDEF="$DEFINES_PATH/el2_pdef.vh"
 PD_DEFINES="$DEFINES_PATH/pd_defines.vh"
+EL2_DEF="$DESIGN_DIR/include/el2_def.sv"
 
 DEFINES_REGEX="s/((\`define)|(\`ifndef)|(\`undef)) ([A-Z0-9_]+).*/\5/p"
 DEFINES_REPLACE_REGEX="s/((\`define)|(\`ifndef)|(\`undef)) ([A-Z0-9_]+)/\1 "$PREFIX"\5/"
 DEFINES="$(sed -nr "$DEFINES_REGEX" $COMMON_DEFINES $PD_DEFINES | sort -ur)"
-DESIGN_FILES="$(find $DESIGN_DIRS -name "*.sv" -o -name "*.vh")"
+DESIGN_FILES="$(find $DESIGN_DIR -name "*.sv" -o -name "*.vh")"
 
 # add prefix to macro names
-sed -E "$DEFINES_REPLACE_REGEX" $COMMON_DEFINES >  $DEFINES_PATH/"$PREFIX"common_defines.vh
-sed -E "$DEFINES_REPLACE_REGEX" $PD_DEFINES >  $DEFINES_PATH/"$PREFIX"pd_defines.vh
+sed -E "$DEFINES_REPLACE_REGEX" $COMMON_DEFINES > $DEFINES_PATH/"$PREFIX"common_defines.vh
+sed -E "$DEFINES_REPLACE_REGEX" $PD_DEFINES > $DEFINES_PATH/"$PREFIX"pd_defines.vh
 
 # replace renamed macros in RTL sources
 for DEFINE in $DEFINES; do
@@ -38,7 +39,7 @@ sed -i "s/include \"el2_pdef.vh\"/include \""$PREFIX"el2_pdef.vh\"/g" $DESIGN_FI
 
 # replace package name and its imports in RTL sources
 sed -i "s/import el2_pkg/import "$PREFIX"el2_pkg/g" $DESIGN_FILES
-sed -i "s/package el2_pkg/package "$PREFIX"el2_pkg/g" ../design/include/el2_def.sv
+sed -i "s/package el2_pkg/package "$PREFIX"el2_pkg/g" $EL2_DEF
 
 MODULES_REGEX="s/^module ([A-Za-z0-9_]+).*/\1/p"
 MODULES="$(sed -nr "$MODULES_REGEX" $DESIGN_FILES | sort -ur)"
@@ -51,6 +52,5 @@ for MODULE in $MODULES; do
     sed -i -E "s/[^A-Za-z0-9_]$MODULE[^A-Za-z0-9_]/"$PREFIX"$MODULE /g" $DESIGN_FILES
 done
 
-# replace toplevel names in verification tests' Makefiles
-MAKEFILES="$(find ../verification -name "Makefile")"
-sed -i -E "s/TOPLEVEL[[:space:]]+=[[:space:]]+([A-Za-z0-9_]+)/TOPLEVEL = "$PREFIX"\1/g" $MAKEFILES
+# remove old header files to avoid redefining their contents during elaboration
+rm $COMMON_DEFINES $EL2_PARAM $EL2_PDEF $PD_DEFINES

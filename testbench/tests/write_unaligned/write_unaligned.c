f1c3d47ec20559b9ff574e7fa4d2a084880b307c
Wojciech Sipak <wsipak@antmicro.com>
2024-10-10 16:37:21 +0200

    check the value of mscause

--- testbench/tests/write_unaligned/write_unaligned.c
+++ testbench/tests/write_unaligned/write_unaligned.c
@@ -8,6 +8,8 @@ int handler() {
 
 int main () {
     int (*func)() = (int (*)())0x70000001;
+    volatile uint32_t csr_value;
+
     printf("jumping to 0x70000001\n");
     func();
     printf("jumping to 0x80001\n");
@@ -22,5 +24,9 @@ int main () {
     printf("jumping to 0xa001\n");
     func = 0xa001;
     func();
+
+    // read CSR at address 0x7FF (mscause)
+    __asm__ volatile ("csrr %0, 0x7FF" : "=r"(csr_value));
+
     return 0;
 }

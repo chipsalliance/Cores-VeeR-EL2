821b1a7026e799c022a00ca98ed3dbe394d11541
Robert Szczepanski <rszczepanski@antmicro.com>
2024-10-25 15:05:35 +0200

    Add proper reset test for DCLS
    Signed-off-by: Robert Szczepanski <rszczepanski@antmicro.com>

--- verification/block/common.mk
+++ verification/block/common.mk
@@ -78,4 +78,4 @@ endif
 
 # Rules for generating VeeR config
 $(CFGDIR)/common_defines.vh:
-	cd $(CURDIR) && $(CONFIG)/veer.config -fpga_optimize=0 $(EXTRA_CONFIG_OPTS)
+	cd $(CURDIR) && $(CONFIG)/veer.config -fpga_optimize=0 $(EXTRA_CONFIG_OPTS) $(EXTRA_VEER_CONFIG)

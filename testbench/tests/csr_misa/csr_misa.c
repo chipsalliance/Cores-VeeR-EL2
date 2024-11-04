7afd3b0aea8f8ebf3c16206c58fb61adcba2f597
Wojciech Sipak <wsipak@antmicro.com>
2024-11-04 13:46:53 +0100

    csr_misa: mask U bit

--- testbench/tests/csr_misa/csr_misa.c
+++ testbench/tests/csr_misa/csr_misa.c
@@ -6,12 +6,21 @@
     res; \
 })
 
-int main () {
+#define U_MODE_MASK (1<<20)
 
-    const unsigned long golden = 0x40101104;
+int main () {
+    unsigned long golden;
 
     // Read and print misa
     unsigned long misa = read_csr(misa);
+
+    // This is to make the test work both with U mode and without U mode.
+    // we're modifying the golden rather than ignoring a bit because Renode tests assume there's a specific string in the output.
+    if (misa & U_MODE_MASK) {
+        golden = 0x40101104;
+    } else {
+        golden = 0x40001104;
+    }
     printf("misa = 0x%08X vs. 0x%08X\n", misa, golden);
 
     // Check

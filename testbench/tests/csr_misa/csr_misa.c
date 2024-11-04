78b21bcd773d7fd7d6cc1ddc6066b39a6c74d050
Wojciech Sipak <wsipak@antmicro.com>
2024-11-04 13:46:53 +0100

    csr_misa: mask U bit

--- testbench/tests/csr_misa/csr_misa.c
+++ testbench/tests/csr_misa/csr_misa.c
@@ -7,11 +7,14 @@
 })
 
 int main () {
+    // bit [20] is set to 0 to skip checking U mode, to make this test work both with U mode and without U mode.
+    const unsigned long check_mask = ~(1 << 20);
 
-    const unsigned long golden = 0x40101104;
+    const unsigned long golden = 0x40101104 & check_mask;
 
     // Read and print misa
-    unsigned long misa = read_csr(misa);
+    unsigned long misa = read_csr(misa) & check_mask;
+
     printf("misa = 0x%08X vs. 0x%08X\n", misa, golden);
 
     // Check

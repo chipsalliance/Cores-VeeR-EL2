19bf0ed978b6c10a0a3d6280cb1347b54e28d474
Wojciech Sipak <wsipak@antmicro.com>
2024-12-30 12:41:53 +0100

    openocd test: simplify test, compare gdb output with reference

--- .github/scripts/gdb_test.sh
+++ .github/scripts/gdb_test.sh
@@ -18,6 +18,8 @@
 
 SIM_LOG=`realpath sim.log`
 OPENOCD_LOG=`realpath openocd.log`
+GDB_LOG=`realpath gdb.log`
+
 if [ -z $GCC_PREFIX ]; then
     GCC_PREFIX=riscv64-unknown-elf
 fi
@@ -34,11 +36,6 @@ export GCC_PREFIX
 
 set +e
 
-if [ "$#" -lt 1 ]; then
-    echo "Usage: gdb_test.sh <command> [args ...]"
-    exit 1
-fi
-
 # Utils
 source `dirname ${BASH_SOURCE[0]}`/utils.sh
 
@@ -50,10 +47,12 @@ terminate_all () {
 }
 
 print_logs () {
-    echo -e "${COLOR_WHITE}======== Simulation log ========${COLOR_OFF}"
-    cat ${SIM_LOG} || true
     echo -e "${COLOR_WHITE}======== OpenOCD log ========${COLOR_OFF}"
     cat ${OPENOCD_LOG} || true
+    echo -e "${COLOR_WHITE}======== Simulation log ========${COLOR_OFF}"
+    cat ${SIM_LOG} || true
+    echo -e "${COLOR_WHITE}======== GDB log ========${COLOR_OFF}"
+    cat ${GDB_LOG} || true
 }
 
 echo -e "${COLOR_WHITE}======== Launching interactive simulation ========${COLOR_OFF}"
@@ -74,7 +73,6 @@ SIM_PID=$!
 
 # Wait
 wait_for_phrase "${SIM_LOG}" "${SIM_START_STRING}"
-# TODO handle proper string in the output instead of waiting
 sleep 1s
 retcode=$?
 if [ $retcode -ne 0 ]; then
@@ -105,10 +103,10 @@ echo -e "OpenOCD running and ready (pid=${OPENOCD_PID})"
 sleep 1s
 
 # Run the test
-echo -e "${COLOR_WHITE}======== Running test '$@' ========${COLOR_OFF}"
+echo -e "${COLOR_WHITE}======== Running GDB script test.gdb ========${COLOR_OFF}"
 
-bash -c "$(printf ' %q' "$@")" > test.log 2>&1 &
-TEST_PID=$!
+${GCC_PREFIX}-gdb -n --batch -x ${RV_ROOT}/.github/scripts/test.gdb > "${GDB_LOG}" &
+GDB_PID=$!
 
 # The simulation must end naturally in order to produce coverage data.
 wait ${SIM_PID}
@@ -116,7 +114,31 @@ wait ${SIM_PID}
 # OpenOCD waits endlessly for the target (Vtb_top) to reconnect.
 # Kill OpenoCD and GDB in case they're stuck
 kill -s SIGKILL ${OPENOCD_PID} || true
-kill -s SIGKILL ${TEST_PID} || true
+kill -s SIGKILL ${GDB_PID} || true
 
 # Display logs
 print_logs
+
+# Parse the log, extract register values. Skip those which change as the
+# program executes since we don't know at which point we tap in.
+grep -E '^ra |^sp |^gp |^tp |^t[01256] |^s[0-9]+ |^a[0-9]+ |^\$[0-9]+ |^\$ |^Hardware |^Breakpoint' "${GDB_LOG}" > gdb_test_dump.txt
+
+gdb_output_golden=${RV_ROOT}/.github/scripts/gdb_test_golden.txt
+grep -q "TEST_PASSED" "${SIM_LOG}"
+tb_passed=$?
+# Compare the dumps
+diff -E -y ${gdb_output_golden} gdb_test_dump.txt
+gdb_output_match=$?
+
+if [ "$tb_passed" -ne 0 ]; then
+    echo "Testbench failed. The test did not write 0xff to the mailbox to indicate success."
+    exit 1
+fi
+echo "Testbench passed."
+
+if [ "$gdb_output_match" -ne 0 ]; then 
+    echo "The output from GDB doesn't match the golden reference. See ${gdb_output_golden}"
+    exit 1
+fi
+echo "The output from GDB matches the golden reference."
+echo "TEST PASSED"

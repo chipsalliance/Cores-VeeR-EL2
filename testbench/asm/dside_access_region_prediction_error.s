50ccd6283fa095d2ddd8c7311009e1870f33ed1e
Wiktoria Kuna <wkuna@antmicro.com>
2025-01-23 16:32:24 +0100

    testbench: dside: Use const address to overflow
    Likely due to RV_PIC_BASE_ADDR value changing,
    the 'dside_access_region_prediction_error' test would
    no longer cause an overflow and hence not cause a prediction error.
    
    Signed-off-by: Wiktoria Kuna <wkuna@antmicro.com>

--- testbench/asm/dside_access_region_prediction_error.s
+++ testbench/asm/dside_access_region_prediction_error.s
@@ -3,11 +3,9 @@
 dside_load_region_prediction_error:
     li x4, 0x5
     li x5, 0x5
-    // this assumes that RV_PIC_BASE_ADDR is as high in the region
-    // as realistically allowed, e.g. 0xffff8000, this allows us
-    // to construct an address that will overflow to another region
-    // when offset is used in an 'lw' instruction: 0xfffffffc + 0x4
-    li x2, RV_PIC_BASE_ADDR + 0x7ffc
+    // We take a large address that will overflow to another region
+    // when offset is used in an 'lw' instruction: 0xFFFFFFFC + 0x4
+    li x2, 0xFFFFFFFC
     lw x2, 0x4(x2)
     j fail_if_not_serviced
 
@@ -15,7 +13,7 @@ dside_store_region_prediction_error:
     li x4, 0x7
     li x5, 0x5
     // same as in load region prediction error
-    li x2, RV_PIC_BASE_ADDR + 0x7ffc
+    li x2, 0xFFFFFFFC
     sw x2, 0x4(x2)
     j fail_if_not_serviced
 

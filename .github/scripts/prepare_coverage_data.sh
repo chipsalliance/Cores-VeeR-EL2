ae6bc392b351c378b264d3b72e46db48deaadc15
Adam Jeliński <ajelinski@antmicro.com>
2025-01-30 10:20:58 +0100

    ci: scripts: Use info-process for all transformations
    It's also now installed via PIP.

--- .github/scripts/prepare_coverage_data.sh
+++ .github/scripts/prepare_coverage_data.sh
@@ -1,49 +1,10 @@
 set -e
 set -o pipefail
 
-cat <<EOF >> preprocess.py
-import sys
-filter = None
-in_file = False
-if sys.argv[2] == "--filter":
-  filter = sys.argv[3]
-print("TN:verilator_coverage")
-with open(sys.argv[1], 'r') as file:
-  for line in file:
-    line = line.replace("\n", "")
-    if line[0:3] == "SF:":
-        if filter == None or line.startswith("SF:%s" % filter):
-            in_file = True
-            print(line)
-            continue
-        else:
-            in_file = False
-            continue
-    if not in_file:
-        continue
-    if "end_of_record" in line:
-        in_file = False
-        print(line)
-        continue
-    if line[0:1] == "#":
-      print(line)
-    elif line[0:3] == "DA:":
-      data = line.split(",")
-      line = "%s,%d" % (data[0], int(data[1]) > 0)
-      print(line)
-    elif line[0:5] == "BRDA:":
-      data = line.split(",")
-      line = "%s,%s,%s,%d" % (data[0],data[1],data[2], int(data[3]) > 0)
-      print(line)
-    else:
-      print(line)
-EOF
+python3 -m venv venv
+. venv/bin/activate
 
-git clone https://github.com/antmicro/info-process
-cd info-process
-git checkout 7c030a4625049726e998065e227e06bac77519d8
-PATH="`pwd`:$PATH"
-cd ..
+pip install git+https://github.com/antmicro/info-process@12d2522f
 
 mkdir info_files
 mv *.info info_files
@@ -51,8 +12,8 @@ tar acf verilator_coverage_single_data.tar.gz info_files
 cd info_files
 ls
 
-ls *_toggle.info | xargs info-merge.py --output coverage_toggle_verilator.info
-ls *_branch.info | xargs info-merge.py --output coverage_line_verilator.info
+ls *_toggle.info | xargs info-process merge --output coverage_toggle_verilator.info
+ls *_branch.info | xargs info-process merge --output coverage_line_verilator.info
 
 cp coverage_toggle_verilator.info ../
 cp coverage_line_verilator.info ../
@@ -63,27 +24,19 @@ rm -rf info_files
 mv coverage_line_verilator.info line.info
 python3 .github/scripts/split_info.py line.info --branch > coverage_branch_verilator.info
 python3 .github/scripts/split_info.py line.info --line > coverage_line_verilator.info
+rm line.info
 
-find . -type f -name 'coverage_*.info' -exec sed -i 's_^SF:.*Cores-VeeR-EL2/_SF:_g' {} \;
+for INFO in *.info
+do
+    info-process transform --strip-file-prefix '.*Cores-VeeR-EL2/' --filter 'design/' --normalize-hit-counts $INFO
 
-python3 preprocess.py coverage_line_verilator.info --filter "design/" > _coverage_line.info
-python3 preprocess.py coverage_toggle_verilator.info --filter "design/" > _coverage_toggle.info
-python3 preprocess.py coverage_branch_verilator.info --filter "design/" > _coverage_branch.info
+    # Filter out `lockstep` and `el2_regfile_if` modules if DCLS tests are not enabled
+    if [ -z "${DCLS_ENABLE}" ]; then
+        info-process transform --filter-out '(lockstep|el2_regfile_if)' $INFO
+    fi
+done
 
-if [ -z "${DCLS_ENABLE}" ]; then
-        # filter out lockstep module, if DCLS tests are not enabled
-        python3 info-process/info-process.py --filter-out 'lockstep' $(realpath _coverage_toggle.info)
-        python3 info-process/info-process.py --filter-out 'lockstep' $(realpath _coverage_line.info)
-        python3 info-process/info-process.py --filter-out 'lockstep' $(realpath _coverage_branch.info)
-
-        python3 info-process/info-process.py --filter-out 'el2_regfile_if' $(realpath _coverage_toggle.info)
-        python3 info-process/info-process.py --filter-out 'el2_regfile_if' $(realpath _coverage_line.info)
-        python3 info-process/info-process.py --filter-out 'el2_regfile_if' $(realpath _coverage_branch.info)
-fi
-
-cp _coverage_line.info coverage_line_verilator.info
-cp _coverage_branch.info coverage_branch_verilator.info
-cp _coverage_toggle.info coverage_toggle_verilator.info
+info-process transform --set-block-ids --add-two-way-toggles --add-missing-brda-entries coverage_toggle_verilator.info
 
 grep 'SF:' coverage_*.info | cut -d ":" -f 3 | sort | uniq > files.txt
 
@@ -106,9 +59,6 @@ export COMMIT=$GITHUB_SHA
                         fi
                 done
 } < files.txt > sources.txt
-
-info-process.py --set-block-ids coverage_toggle_verilator.info
-info-process.py --add-two-way-toggles --add-missing-brda-entries coverage_toggle_verilator.info
 
 mkdir test_data
 cp coverage_line_*.info coverage_toggle_*.info coverage_branch_* sources.txt test_data

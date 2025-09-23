61f56e72679a31cb414bbbaeae26ea5c5dee9f8f
Maciej Dudek <mdudek@antmicro.com>
2025-09-23 13:53:21 +0200

    Move lockstep structs to pacakge Fix bind
    Signed-off-by: Maciej Dudek <mdudek@antmicro.com>

--- verification/block/dcls/el2_veer_lockstep_cov_bind.sv
+++ verification/block/dcls/el2_veer_lockstep_cov_bind.sv
@@ -1,5 +1,5 @@
 module el2_veer_lockstep_cov_bind ();
      `ifdef FCOV
-     bind el2_veer_lockstep_cov_if(.*);
+     bind el2_veer_lockstep el2_veer_lockstep_cov_if el2_veer_lockstep_cov(.*);
      `endif
 endmodule

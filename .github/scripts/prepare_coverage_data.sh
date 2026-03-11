2af65d27b444d9ee829d3ffd1ffe726d1df7c2ef
Tomasz Worobiec <tworobiec@antmicro.com>
2026-03-11 14:42:53 +0100

    Add report coverage job

--- .github/scripts/prepare_coverage_data.sh
+++ .github/scripts/prepare_coverage_data.sh
@@ -1,172 +1,66 @@
-set -e
+#!/bin/bash
+
+set -eu
 set -o pipefail
 
-cat <<EOF >> preprocess.py
-import sys
-filter = None
-in_file = False
-if sys.argv[2] == "--filter":
-  filter = sys.argv[3]
-print("TN:verilator_coverage")
-with open(sys.argv[1], 'r') as file:
-  for line in file:
-    line = line.replace("\n", "")
-    if line[0:3] == "SF:":
-        if filter == None or line.startswith("SF:%s" % filter):
-            in_file = True
-            print(line)
-            continue
-        else:
-            in_file = False
-            continue
-    if not in_file:
-        continue
-    if "end_of_record" in line:
-        in_file = False
-        print(line)
-        continue
-    if line[0:1] == "#":
-      print(line)
-    elif line[0:3] == "DA:":
-      data = line.split(",")
-      line = "%s,%d" % (data[0], int(data[1]) > 0)
-      print(line)
-    elif line[0:5] == "BRDA:":
-      data = line.split(",")
-      line = "%s,%s,%s,%d" % (data[0],data[1],data[2], int(data[3]) > 0)
-      print(line)
-    else:
-      print(line)
-EOF
+if [ -v CI ]
+then
+  set -x
 
-cat <<EOF >> brda_filter.py
-def filter(infile, outfile):
-    out = open(outfile, 'w')
+  apt update
+  apt install -y xz-utils
 
-    with open(infile) as f:
-        lines = f.readlines()
-        n = 0
+  # XZ compresses .info files A LOT better than GZIP, e.g., 9.4MB vs 175MB.
+  tar acf ${SIM}_coverage_single_data.tar.xz info_files_$SIM
+  ls info_files_$SIM
+fi
 
-        while n < len(lines):
-            hits = {}
-            if 'BRDA' in lines[n]:
-                data = lines[n].split(",")[2].split("_")
-                lineno = lines[n].split(",")[0]
-                typ = data[0]
-                key = data[1]
-                hit = int(data[2])
-                val = int(lines[n].split(",")[3])
+DB_COUNT=`ls info_files_$SIM | sed 's#_[^_]*.info##' | sort | uniq | wc -l`
 
-                hits[lineno] = {}
-                hits[lineno][typ] = {}
-                hits[lineno][typ][key] = {}
-                hits[lineno][typ][key][0] = []
-                hits[lineno][typ][key][1] = []
-                hits[lineno][typ][key][val].append(hit)
+# Filter out lockstep and el2_regfile_if modules if DCLS tests are not enabled
+if [ -v DCLS_ENABLE ]
+then
+    _filter_out=''
+else
+    _filter_out='--filter-out (lockstep|el2_regfile_if)'
+fi
 
-                n += 1
-                while lines[n].startswith(lineno):
-                    lineno = lines[n].split(",")[0]
-                    data = lines[n].split(",")[2].split("_")
-                    typ = data[0]
-                    key = data[1]
-                    hit = int(data[2])
-                    val = int(lines[n].split(",")[3])
+# Source path transformations are needed before merging to have matching paths in `.desc` files.
+find info_files_$SIM -name '*.info' -exec info-process transform \
+    --strip-file-prefix '.*Cores-VeeR-EL2/' \
+    --filter 'design/' $_filter_out \
+    {} \;
 
-                    if typ not in hits[lineno]:
-                        hits[lineno][typ] = {}
+if [ $SIM = verilator ]
+then
+    # Split branch and line before merging to have correct data in `.desc` files.
+    for FILE in info_files_$SIM/*_branch.info
+    do
+        info-process extract --coverage-type line --output ${FILE%%_branch.info}_line.info $FILE
+        info-process extract --coverage-type cond --output ${FILE%%_branch.info}_cond.info $FILE
+        # Extract branch coverage last, so that it can happen in-place
+        info-process extract --coverage-type branch --output $FILE $FILE
+    done
+fi
 
-                    if key not in hits[lineno][typ]:
-                        hits[lineno][typ][key] = {}
-                        hits[lineno][typ][key][0] = []
-                        hits[lineno][typ][key][1] = []
+for TYPE in branch line toggle cond
+do
+    _sort_opt=''
+    _transform_extra_opts=''
+    if [ $SIM = verilator ] && [ $TYPE = toggle ]
+    then
+        _sort_opt=--sort-brda-names
+        _transform_extra_opts='--set-block-ids --add-two-way-toggles --add-missing-brda-entries'
+    fi
 
-                    if hit not in hits[lineno][typ][key][val]:
-                        hits[lineno][typ][key][val].append(hit)
-                    n += 1
+    info-process merge --output coverage_${TYPE}_$SIM.info $_sort_opt \
+        --test-list tests_${TYPE}_$SIM.desc --test-list-strip coverage_,_$TYPE.info \
+        info_files_$SIM/*_$TYPE.info
 
-                # normalize hits
-                for h in hits:
-                    for t in hits[h]:
-                        for l in hits[h][t]:
-                            all_bits = hits[h][t][l][0] + hits[h][t][l][1]
-                            try:
-                                mi = min(all_bits)
-                                for v in [0, 1]:
-                                    for i, k in enumerate(hits[h][t][l][v]):
-                                        hits[h][t][l][v][i] -= mi
-                            except ValueError:
-                                # allow empty lists (no hits or misses)
-                                pass
+    info-process transform --normalize-hit-counts $_transform_extra_opts coverage_${TYPE}_$SIM.info
+done
 
-                new_hits = {}
-
-                for h in hits:
-                    for t in hits[h]:
-                        for l in hits[h][t]:
-                            for v in [0, 1]:
-                                for k in hits[h][t][l][v]:
-                                    if t not in new_hits:
-                                       new_hits[t] = {}
-                                    if not k in new_hits[t] or new_hits[t][k] != 1:
-                                        new_hits[t][k] = v
-
-                for t in new_hits.keys():
-                    for k in sorted(new_hits[t].keys()):
-                        if len(new_hits[t].keys()) > 1:
-                            out.write(f"{lineno},0,{t}_{k},{new_hits[t][k]}\n")
-                        else:
-                            out.write(f"{lineno},0,{t},{new_hits[t][k]}\n")
-            else:
-                out.write(lines[n])
-                n += 1
-
-    out.close()
-
-filter('coverage_branch_verilator.info',
-       'coverage_branch_verilator_filtered.info')
-
-filter('coverage_toggle_verilator.info',
-       'coverage_toggle_verilator_filtered.info')
-EOF
-
-mkdir info_files
-mv *.info info_files
-cd info_files
-git clone https://github.com/linux-test-project/lcov -b v2.3-beta
-PATH="`pwd`/lcov/bin:$PATH"
-
-ls *_toggle.info | xargs printf -- '-a %s\n' | xargs echo | awk '{ print "lcov "$0" --ignore-errors inconsistent --rc lcov_branch_coverage=1 -o coverage_toggle_verilator.info" }' | bash
-ls *_branch.info | xargs printf -- '-a %s\n' | xargs echo | awk '{ print "lcov "$0" --ignore-errors inconsistent --rc lcov_branch_coverage=1 -o coverage_line_verilator.info" }' | bash
-
-cp coverage_toggle_verilator.info ../
-cp coverage_line_verilator.info ../
-
-cd ../
-rm -rf info_files
-
-mv coverage_line_verilator.info line.info
-python3 .github/scripts/split_info.py line.info --branch > coverage_branch_verilator.info
-python3 .github/scripts/split_info.py line.info --line > coverage_line_verilator.info
-
-find . -type f -name 'coverage_*.info' -exec sed -i 's_^SF:.*Cores-VeeR-EL2/_SF:_g' {} \;
-
-python3 preprocess.py coverage_line_verilator.info --filter "design/" > _coverage_line.info
-python3 preprocess.py coverage_toggle_verilator.info --filter "design/" > _coverage_toggle.info
-python3 preprocess.py coverage_branch_verilator.info --filter "design/" > _coverage_branch.info
-
-cp _coverage_line.info coverage_line_verilator.info
-cp _coverage_branch.info coverage_branch_verilator.info
-cp _coverage_toggle.info coverage_toggle_verilator.info
-
-python3 brda_filter.py
-mv coverage_toggle_verilator.info coverage_toggle_verilator_orig.info_
-mv coverage_toggle_verilator_filtered.info coverage_toggle_verilator.info
-
-mv coverage_branch_verilator.info coverage_branch_verilator_orig.info_
-mv coverage_branch_verilator_filtered.info coverage_branch_verilator.info
-
-grep 'SF:' coverage_*.info | cut -d ":" -f 3 | sort | uniq > files.txt
+rm -rf info_files_$SIM
 
 if [ -z "${GITHUB_HEAD_REF}" ]; then
         # We're in merge triggered run
@@ -176,35 +70,30 @@ else
         export BRANCH=$GITHUB_HEAD_REF
 fi
 export COMMIT=$GITHUB_SHA
-{
-        while read file
-                do
-                        if [ -f $file ]; then
-                                echo "### FILE: $file"
-                                cat "$file"
-                        else
-                                echo "### SKIPPING: $file"
-                        fi
-                done
-} < files.txt > sources.txt
 
-mkdir test_data
-cp coverage_line_*.info coverage_toggle_*.info coverage_branch_* sources.txt test_data
+# Add config.json template, "datasets" will be generated by info-process.
+cat <<END >config.json
+{
+  "title": "VeeR EL2 coverage dashboard",
+  "commit": "$COMMIT",
+  "branch": "$BRANCH",
+  "repo": "cores-veer-el2",
+  "timestamp": "`date +"%Y-%m-%dT%H:%M:%S.%3N%z"`",
+  "additional": {
+    "db_count": "$DB_COUNT",
+    "run_id": "$GITHUB_RUN_ID"
+  }
+}
+END
+
+_out_dir=data_$SIM
+info-process pack --output $_out_dir --config config.json \
+    --coverage-files *_$SIM.info --description-files *_$SIM.desc
+rm config.json *_$SIM.info *_$SIM.desc
 
 # add logo
-cp docs/dashboard-styles/assets/chips-alliance-logo-mono.svg test_data/logo.svg
+cp docs/dashboard-styles/assets/chips-alliance-logo-mono.svg $_out_dir/logo.svg
 
-# add config.json
-echo -n '{ "datasets": { "verilator": { "line": "coverage_line_verilator.info", "branch": "coverage_branch_verilator.info", "toggle": "coverage_toggle_verilator.info" } }, "title": "VeeR EL2 coverage dashboard", "commit": "' > test_data/config.json
-echo -n $COMMIT >> test_data/config.json
-echo -n '", "branch": "' >> test_data/config.json
-echo -n $BRANCH >> test_data/config.json
-echo -n '", "repo": "cores-veer-el2", "timestamp": "' >> test_data/config.json
-echo -n `date +"%Y-%m-%dT%H:%M:%S.%3N%z"` >> test_data/config.json
-echo -n '" }' >> test_data/config.json
+cat $_out_dir/config.json
 
-cat test_data/config.json
-
-cd test_data
-zip ../data.zip *
-cd ..
+echo "Coverage data ready to be packaged in $PWD/$_out_dir"

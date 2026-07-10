0564dde957099d7eb4d1af10cc6718a29f3bb012
Maciej Kurc <mkurc@antmicro.com>
2026-07-10 15:37:46 +0200

    Add CSR recovery backdoor test
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- verification/block/dec/test_dec.py
+++ verification/block/dec/test_dec.py
@@ -2,20 +2,23 @@
 # SPDX-License-Identifier: Apache-2.0
 import pyuvm
 from pyuvm import ConfigDB
-from testbench import BaseEnv, BaseTest, DecSequence, DecTmrRecoverySequence
+from testbench import BaseEnv, BaseTest, DecSequence
+from testbench import DecTmrGprRecoverySequence, DecTmrCsrRecoverySequence
 
 # =============================================================================
 
 
 class DecTluCtlTest(BaseTest):
-    def __init__(self, test_name, name, parent, env_class=BaseEnv):
+    def __init__(self, test_name, name, parent, env_class=BaseEnv, seq_class=DecSequence, seq_kwarg=None):
         self.test_name = test_name
+        self.seq_class = seq_class
+        self.seq_kwarg = dict() if seq_kwarg is None else seq_kwarg
         super().__init__(name, parent, env_class)
+        ConfigDB().set(None, "*", "TEST", self.test_name)
 
     def end_of_elaboration_phase(self):
         super().end_of_elaboration_phase()
-        ConfigDB().set(None, "*", "TEST", self.test_name)
-        self.seq = DecSequence("stimulus")
+        self.seq = self.seq_class("stimulus", **self.seq_kwarg)
 
     async def run(self):
         await self.seq.start(self.env.dec_seqr)
@@ -57,30 +60,25 @@ class TestMeicidpl(DecTluCtlTest):
         super().__init__("meicidpl", name, parent, env_class)
 
 @pyuvm.test()
-class TestTmrRetrieve(BaseTest):
+class TestTmrGprRetrieve(DecTluCtlTest):
     def __init__(self, name, parent, env_class=BaseEnv):
-        super().__init__(name, parent, env_class)
-        self.test_name = "recovery_gpr_access"
-
-    def end_of_elaboration_phase(self):
-        super().end_of_elaboration_phase()
-        ConfigDB().set(None, "*", "TEST", self.test_name)
-        self.seq = DecTmrRecoverySequence("stimulus", "retrieve")
-
-    async def run(self):
-        await self.seq.start(self.env.dec_seqr)
+        super().__init__("recovery_gpr_access", name, parent, env_class,
+            seq_class=DecTmrGprRecoverySequence, seq_kwarg={"mode": "retrieve"})
 
 @pyuvm.test()
-class TestTmrRestore(BaseTest):
+class TestTmrGprRestore(DecTluCtlTest):
     def __init__(self, name, parent, env_class=BaseEnv):
-        super().__init__(name, parent, env_class)
-        self.test_name = "recovery_gpr_access"
+        super().__init__("recovery_gpr_access", name, parent, env_class,
+            seq_class=DecTmrGprRecoverySequence, seq_kwarg={"mode": "restore"})
 
-    def end_of_elaboration_phase(self):
-        super().end_of_elaboration_phase()
-        ConfigDB().set(None, "*", "TEST", self.test_name)
-        self.seq = DecTmrRecoverySequence("stimulus", "restore")
+@pyuvm.test()
+class TestTmrCsrRetrieve(DecTluCtlTest):
+    def __init__(self, name, parent, env_class=BaseEnv):
+        super().__init__("recovery_csr_access", name, parent, env_class,
+            seq_class=DecTmrCsrRecoverySequence, seq_kwarg={"mode": "retrieve"})
 
-    async def run(self):
-        await self.seq.start(self.env.dec_seqr)
-
+@pyuvm.test()
+class TestTmrCsrRestore(DecTluCtlTest):
+    def __init__(self, name, parent, env_class=BaseEnv):
+        super().__init__("recovery_csr_access", name, parent, env_class,
+            seq_class=DecTmrCsrRecoverySequence, seq_kwarg={"mode": "restore"})

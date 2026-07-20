77bcdd20dd9b9237ead2e9b4164aeea0b80d46d6
Maciej Kurc <mkurc@antmicro.com>
2026-07-20 10:30:14 +0200

    TMR AXI interface test
    Internal-tag: [#101072]
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

---
+++ verification/block/tmr_axi/axi_bus.py
@@ -0,0 +1,106 @@
+#
+# Copyright (c) 2026 Antmicro
+# SPDX-License-Identifier: Apache-2.0
+
+from cocotbext.axi.stream import StreamBus
+from cocotbext.axi import AxiAWBus, AxiWBus, AxiBBus, AxiARBus, AxiRBus
+from cocotbext.axi import AxiBus, AxiWriteBus, AxiReadBus
+
+# ==============================================================================
+
+SAxiAWBus = type("SAxiAWBus", (StreamBus,), {
+    "_signals"         : {s: s + ("_o" if "ready" in s else "_i") for s in AxiAWBus._signals},
+    "_optional_signals": {s: s + ("_o" if "ready" in s else "_i") for s in AxiAWBus._optional_signals},
+})
+
+SAxiWBus = type("SAxiWBus", (StreamBus,), {
+    "_signals"         : {s: s + ("_o" if "ready" in s else "_i") for s in AxiWBus._signals},
+    "_optional_signals": {s: s + ("_o" if "ready" in s else "_i") for s in AxiWBus._optional_signals},
+})
+
+SAxiBBus = type("SAxiBBus", (StreamBus,), {
+    "_signals"         : {s: s + ("_i" if "ready" in s else "_o") for s in AxiBBus._signals},
+    "_optional_signals": {s: s + ("_i" if "ready" in s else "_o") for s in AxiBBus._optional_signals},
+})
+
+class SAxiWriteBus(AxiWriteBus):
+    @classmethod
+    def from_prefix(cls, entity, prefix, **kwargs):
+        aw = SAxiAWBus.from_prefix(entity, prefix, **kwargs)
+        w = SAxiWBus.from_prefix(entity, prefix, **kwargs)
+        b = SAxiBBus.from_prefix(entity, prefix, **kwargs)
+        return cls(aw, w, b)
+
+SAxiARBus = type("SAxiARBus", (StreamBus,), {
+    "_signals"         : {s: s + ("_o" if "ready" in s else "_i") for s in AxiARBus._signals},
+    "_optional_signals": {s: s + ("_o" if "ready" in s else "_i") for s in AxiARBus._optional_signals},
+})
+
+SAxiRBus = type("SAxiRBus", (StreamBus,), {
+    "_signals"         : {s: s + ("_i" if "ready" in s else "_o") for s in AxiRBus._signals},
+    "_optional_signals": {s: s + ("_i" if "ready" in s else "_o") for s in AxiRBus._optional_signals},
+})
+
+class SAxiReadBus(AxiReadBus):
+    @classmethod
+    def from_prefix(cls, entity, prefix, **kwargs):
+        ar = SAxiARBus.from_prefix(entity, prefix, **kwargs)
+        r = SAxiRBus.from_prefix(entity, prefix, **kwargs)
+        return cls(ar, r)
+
+class SAxiBus(AxiBus):
+    @classmethod
+    def from_prefix(cls, entity, prefix, **kwargs):
+        write = SAxiWriteBus.from_prefix(entity, prefix, **kwargs)
+        read = SAxiReadBus.from_prefix(entity, prefix, **kwargs)
+        return cls(write, read)
+
+# ==============================================================================
+
+
+MAxiAWBus = type("MAxiAWBus", (StreamBus,), {
+    "_signals"         : {s: s + ("_i" if "ready" in s else "_o") for s in AxiAWBus._signals},
+    "_optional_signals": {s: s + ("_i" if "ready" in s else "_o") for s in AxiAWBus._optional_signals},
+})
+
+MAxiWBus = type("MAxiWBus", (StreamBus,), {
+    "_signals"         : {s: s + ("_i" if "ready" in s else "_o") for s in AxiWBus._signals},
+    "_optional_signals": {s: s + ("_i" if "ready" in s else "_o") for s in AxiWBus._optional_signals},
+})
+
+MAxiBBus = type("MAxiBBus", (StreamBus,), {
+    "_signals"         : {s: s + ("_o" if "ready" in s else "_i") for s in AxiBBus._signals},
+    "_optional_signals": {s: s + ("_o" if "ready" in s else "_i") for s in AxiBBus._optional_signals},
+})
+
+class MAxiWriteBus(AxiWriteBus):
+    @classmethod
+    def from_prefix(cls, entity, prefix, **kwargs):
+        aw = MAxiAWBus.from_prefix(entity, prefix, **kwargs)
+        w = MAxiWBus.from_prefix(entity, prefix, **kwargs)
+        b = MAxiBBus.from_prefix(entity, prefix, **kwargs)
+        return cls(aw, w, b)
+
+MAxiARBus = type("MAxiARBus", (StreamBus,), {
+    "_signals"         : {s: s + ("_i" if "ready" in s else "_o") for s in AxiARBus._signals},
+    "_optional_signals": {s: s + ("_i" if "ready" in s else "_o") for s in AxiARBus._optional_signals},
+})
+
+MAxiRBus = type("MAxiRBus", (StreamBus,), {
+    "_signals"         : {s: s + ("_o" if "ready" in s else "_i") for s in AxiRBus._signals},
+    "_optional_signals": {s: s + ("_o" if "ready" in s else "_i") for s in AxiRBus._optional_signals},
+})
+
+class MAxiReadBus(AxiReadBus):
+    @classmethod
+    def from_prefix(cls, entity, prefix, **kwargs):
+        ar = MAxiARBus.from_prefix(entity, prefix, **kwargs)
+        r = MAxiRBus.from_prefix(entity, prefix, **kwargs)
+        return cls(ar, r)
+
+class MAxiBus(AxiBus):
+    @classmethod
+    def from_prefix(cls, entity, prefix, **kwargs):
+        write = MAxiWriteBus.from_prefix(entity, prefix, **kwargs)
+        read = MAxiReadBus.from_prefix(entity, prefix, **kwargs)
+        return cls(write, read)
---
+++ verification/block/tmr_axi/test_faults.py
@@ -0,0 +1,207 @@
+# Copyright (c) 2026 Antmicro <www.antmicro.com>
+# SPDX-License-Identifier: Apache-2.0
+import random
+from copy import deepcopy
+
+import cocotb
+from cocotb.triggers import Timer, Combine, ClockCycles
+from pyuvm import ConfigDB, test, uvm_sequence
+
+from cocotbext.axi import AxiBurstType
+from axi_agent import AxiWriteTransaction, AxiReadTransaction
+from axi_agent import AxiTransaction, AxiTransactionType
+
+from testbench import (
+    BaseTest,
+    BaseScoreboard,
+    MuBiFalse,
+    MuBiTrue,
+)
+
+# =============================================================================
+
+class FaultScoreboard(BaseScoreboard):
+
+    def check_phase(self):
+
+        events = []
+
+        # Collect all AXI items
+        self.passed = True
+        while self.m_axi_tr_port.can_get():
+
+            if not self.s_axi_a_tr_port.can_get():
+                self.logger.error("No more transactions observed on AXI A port")
+                self.passed = False
+                break
+
+            if not self.s_axi_b_tr_port.can_get():
+                self.logger.error("No more transactions observed on AXI B port")
+                self.passed = False
+                break
+
+            if not self.s_axi_c_tr_port.can_get():
+                self.logger.error("No more transactions observed on AXI C port")
+                self.passed = False
+                break
+
+            _, tr_m = self.m_axi_tr_port.try_get()
+            _, tr_a = self.s_axi_a_tr_port.try_get()
+            _, tr_b = self.s_axi_b_tr_port.try_get()
+            _, tr_c = self.s_axi_c_tr_port.try_get()
+
+            if isinstance(tr_m, AxiWriteTransaction):
+                timestamp = tr_m.b.timestamp
+            elif isinstance(tr_m, AxiReadTransaction):
+                timestamp = tr_m.r[-1].timestamp
+            else:
+                assert False, type(tr_m)
+
+            event = (timestamp, {"m": tr_m, "a": tr_a, "b": tr_b, "c": tr_c})
+            events.append(event)
+
+        # Collect all fault items
+        while self.fault_port.can_get():
+            _, fault = self.fault_port.try_get()
+
+            event = (fault.timestamp, {"fault": fault})
+            events.append(event)
+
+        # Sort by timestamps
+        events.sort(key=lambda ev: ev[0])
+
+        # Check
+        fault = [False, False, False]
+        for timestamp, event in events:
+
+            # Update fault state
+            if "fault" in event:
+                fault = event["fault"].fault
+                continue
+
+            # Get a correct AXI transaction basing of the current indicated
+            # fault state
+            if fault == [False, False, False]:
+                s_axi = event["a"]
+            elif fault == [True, False, False]:
+                s_axi = event["b"]
+            elif fault == [False, True, False]:
+                s_axi = event["c"]
+            elif fault == [False, False, True]:
+                s_axi = event["a"]
+            else:
+                # TODO: Handle the case, currently should not happen in this
+                # test
+                assert False, fault
+
+            # Compare
+            if event["m"] != s_axi:
+                self.logger.error(f"AXI transaction mismatch at {timestamp}ps")
+                self.logger.error(f" Fault: {str(fault)}")
+
+                for ev in ["a", "b", "c", "m"]:
+                    self.logger.error(f" AXI {ev.upper()}:")
+                    for l in str(event[ev]).splitlines():
+                        self.logger.error("  " + l)
+
+                self.passed = False
+
+# =============================================================================
+
+class TransactionSequence(uvm_sequence):
+    """
+    A sequence which plays the given list of items
+    """
+
+    def __init__(self, name, items, seqr):
+        self.items = items
+        self.seqr  = seqr
+        super().__init__(name)
+
+    async def body(self):
+        for item in self.items:
+            await self.seqr.start_item(item)
+            await self.seqr.finish_item(item)
+
+# =============================================================================
+
+@test()
+class TestFaults(BaseTest):
+    def __init__(self, name, parent):
+        super().__init__(name, parent, FaultScoreboard)
+
+    async def run(self):
+        period     = ConfigDB().get(None, "", "TEST_CLK_PERIOD")
+        iterations = ConfigDB().get(None, "", "TEST_ITERATIONS")
+
+        for it in range(iterations):
+
+            # Generate random items
+            base_items = []
+            for i in range(20):
+                item = AxiTransaction()
+                item.type    = random.choice(list(AxiTransactionType))
+                item.address = random.randrange(1 << 32)
+                item.id      = random.randrange(1 << 4)
+
+                if item.type == AxiTransactionType.WRITE:
+                    item.data = bytearray([random.randrange(256) for j in range(8)])
+
+                elif item.type == AxiTransactionType.READ:
+                    item.length = 1
+
+                else:
+                    assert False, item.type
+
+                base_items.append(item)
+
+            # Replicate and inject discrepancies
+            fault = random.randrange(3)
+            items = []
+
+            for i in range(3):
+                core_items = deepcopy(base_items)
+                items.append(core_items)
+
+                if i == fault:
+                    which = random.randrange(len(core_items))
+
+                    # opts = ["addr", "burst"]
+                    opts = ["addr"]
+                    if core_items[which].type == AxiTransactionType.WRITE:
+                        opts.append("data")
+
+                    what = random.choice(opts)
+
+                    if what == "addr":
+                        core_items[which].address ^= 1 << random.randrange(32)
+
+                    elif what == "data":
+                        j = random.randrange(len(core_items[which].data))
+                        core_items[which].data[j] ^= 1 << random.randrange(8)
+
+                    #elif what == "id":
+                    #    core_items[which].id ^= 1 << random.randrange(4)
+
+                    elif what == "burst":
+                        core_items[which].burst = random.choice(list(AxiBurstType))
+
+                    else:
+                        assert False, what
+
+            # Get sequencers
+            sequencers = [ConfigDB().get(None, "", "AXI_AGENT_" + i).sequencer \
+                          for i in ["A", "B", "C"]]
+
+            # Create sequences
+            sequences = [TransactionSequence("seq_" + i, it, s) \
+                         for i, it, s in zip(["a", "b", "c"], items, sequencers)]
+
+            # Start tasks
+            tasks = [cocotb.start_soon(s.start()) for s in sequences]
+            timer = Timer(20 * period)
+
+            await Combine(*tasks, timer)
+
+            # Reset
+            await self.reset()

8643abe89bad7bdfdd011773011fb0f5ccaeb13d
Tomasz Michalak <tmichalak@antmicro.com>
2026-07-28 15:56:31 +0200

    Add copyright headers

--- design/lib/el2_assert.sv
+++ design/lib/el2_assert.sv
@@ -1,3 +1,18 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026 Antmicro <www.antmicro.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 // Implementation derived from https://github.com/lowRISC/opentitan/blob/891c6079b2f19650f2bb6d248c28ea4cfbd746d2/hw/ip/prim/rtl/prim_assert.sv#L48
 
 // Static assertions for checks inside SV packages. If the conditions is not true, this will

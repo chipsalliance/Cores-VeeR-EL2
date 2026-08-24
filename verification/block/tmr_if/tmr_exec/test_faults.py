12633edd6fa72c85418967fe7c501f3185e0e766
Maciej Kurc <mkurc@antmicro.com>
2026-08-24 14:29:00 +0200

    Add test for TMR execution voters
    Internal-tag: [#103711]
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

---
+++ verification/block/tmr_if/tmr_exec/test_faults.py
@@ -0,0 +1,201 @@
+# Copyright (c) 2026 Antmicro <www.antmicro.com>
+# SPDX-License-Identifier: Apache-2.0
+import random
+from collections import defaultdict
+from copy import deepcopy
+
+import cocotb
+from cocotb.triggers import ClockCycles, Combine, Timer
+from pyuvm import ConfigDB, test, uvm_sequence
+from testbench import (
+    BaseScoreboard,
+    BaseTest,
+)
+
+from common import (
+    BusItem,
+    MuBiFalse,
+    MuBiTrue,
+)
+
+# =============================================================================
+
+
+class BusSequence(uvm_sequence):
+    """
+    A generic bus sequence which blindly drives bus states
+    """
+
+    def __init__(self, name, seqr):
+        self.items = []
+        self.seqr = seqr
+        super().__init__(name)
+
+    async def body(self):
+        for item in self.items:
+            await self.seqr.start_item(item)
+            await self.seqr.finish_item(item)
+
+
+# =============================================================================
+
+
+class FaultScoreboard(BaseScoreboard):
+    """
+    The scoreboard collects bus' state change items, recovers bus state,
+    predicts output state via majority voting and fault state, compares with
+    observation.
+    """
+
+    def check_phase(self):
+        events = []
+        period = 1000 * ConfigDB().get(None, "", "TEST_CLK_PERIOD")  # [ps]
+
+        # Collect all veer bus items
+        while self.veer_bus_port.can_get():
+            _, item = self.veer_bus_port.try_get()
+
+            event = (item.timestamp + period, {"veer": item})
+            events.append(event)
+
+        # Collect all out bus items
+        while self.out_bus_port.can_get():
+            _, item = self.out_bus_port.try_get()
+
+            event = (item.timestamp + period, {"out": item})
+            events.append(event)
+
+        # Collect all fault items
+        while self.fault_port.can_get():
+            _, fault = self.fault_port.try_get()
+
+            event = (fault.timestamp, {"fault": fault})
+            events.append(event)
+
+        # Unique sorted timestamps
+        timestamps = set([ev[0] for ev in events])
+        timestamps = sorted(list(timestamps))
+
+        # Group by timestamps
+        ts_events = defaultdict(lambda: list())
+        for ev in events:
+            ts_events[ev[0]].append(ev[1])
+
+        # Check
+        fault_state = [False, False, False]
+        veer_bus_state = None
+        out_bus_state = None
+
+        self.passed = True
+        for timestamp in timestamps:
+
+            # Update state
+            for event in ts_events[timestamp]:
+                if "fault" in event:
+                    fault_state = event["fault"].fault
+                if "veer" in event:
+                    veer_bus_state = event["veer"].signals
+                if "out" in event:
+                    out_bus_state = event["out"].signals
+
+            if veer_bus_state is None or out_bus_state is None:
+                continue
+
+            # Predict
+            if fault_state == [False, False, False]:
+                index = 0  # Arbitrary
+            elif fault_state == [True, False, False]:
+                index = 1
+            elif fault_state == [False, True, False]:
+                index = 2
+            elif fault_state == [False, False, True]:
+                index = 0
+            else:
+                # TODO: Handle the case, currently should not happen in this
+                # test
+                assert False, fault_state
+
+            pred_bus_state = {k.replace("_veer", ""): v[index] for k, v in veer_bus_state.items()}
+
+            # Compare
+            if pred_bus_state != out_bus_state:
+                self.passed = False
+
+                self.logger.error(f"Bus state mismatch at {timestamp}ps")
+                self.logger.error(f" Fault: {str(fault_state)}")
+
+                def sig2str(d):
+                    keys = sorted(list(d.keys()))
+                    return ", ".join([f"{k}={d[k]}" for k in keys])
+
+                self.logger.error(f" Out  : {sig2str(out_bus_state)}")
+                self.logger.error(f" Pred.: {sig2str(pred_bus_state)}")
+
+
+# =============================================================================
+
+
+@test()
+class TestFaults(BaseTest):
+    """
+    Drives randomized state on the input buses occassionally upsetting one
+    signal.
+    """
+
+    def __init__(self, name, parent):
+        super().__init__(name, parent, FaultScoreboard)
+
+    def end_of_elaboration_phase(self):
+        super().end_of_elaboration_phase()
+        self.seq = BusSequence("seq", self.env.veer_bus_seqr)
+
+    async def run(self):
+        iterations = ConfigDB().get(None, "", "TEST_ITERATIONS")
+
+        signals = {
+            "o_cpu_halt_ack_veer": None,
+            "o_cpu_run_ack_veer": None,
+            "o_cpu_halt_status_veer": None,
+            "o_debug_mode_status_veer": None,
+            "mpc_debug_halt_ack_veer": None,
+            "mpc_debug_run_ack_veer": None,
+            "debug_brkpt_status_veer": None,
+        }
+
+        # Get signal lengths
+        for name in signals:
+            sig = getattr(cocotb.top, name + "[0]")
+            signals[name] = len(sig)
+
+        # Run test iterations
+        for it in range(iterations):
+
+            # Generate random items
+            items = []
+            for i in range(20):
+                item = BusItem()
+
+                for name in signals:
+                    value = random.randrange(1 << signals[name])
+                    for j in range(3):
+                        item.signals[name + f"[{j}]"] = value
+
+                items.append(item)
+
+            # Inject discrepancies
+            fault = random.randrange(3)
+            what = random.choice(list(signals.keys()))
+
+            # Don't upset the last transaction. The upset would last during
+            # reset which clears the fault state making the scoreboard report
+            # error.
+            which = random.randrange(len(items) - 1)
+
+            items[which].signals[what + f"[{fault}]"] ^= 1 << random.randrange(signals[what])
+
+            # Run the sequence
+            self.seq.items = items
+            await self.seq.start()
+
+            # Reset
+            await self.reset()
---
+++ verification/block/tmr_if/tmr_exec/testbench.py
@@ -0,0 +1,163 @@
+#
+# Copyright (c) 2026 Antmicro
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+
+from cocotb.utils import get_sim_time
+from pyuvm import *
+
+import common
+from common import BusDriver, BusItem, BusMonitor, FaultItem, FaultMonitor, MuBiFalse
+
+# ==============================================================================
+
+
+class BaseScoreboard(uvm_component):
+    """
+    Base PyUVM scoreboard
+    """
+
+    def build_phase(self):
+        self.passed = False
+
+        self.veer_bus_fifo = uvm_tlm_analysis_fifo("veer_bus_fifo", self)
+        self.veer_bus_port = uvm_get_port("veer_bus_port", self)
+
+        self.out_bus_fifo = uvm_tlm_analysis_fifo("out_bus_fifo", self)
+        self.out_bus_port = uvm_get_port("out_bus_port", self)
+
+        self.fault_fifo = uvm_tlm_analysis_fifo("fault_fifo", self)
+        self.fault_port = uvm_get_port("fault_port", self)
+
+    def connect_phase(self):
+        self.veer_bus_port.connect(self.veer_bus_fifo.get_export)
+        self.out_bus_port.connect(self.out_bus_fifo.get_export)
+        self.fault_port.connect(self.fault_fifo.get_export)
+
+    def check_phase(self):
+        raise NotImplementedError()
+
+    def final_phase(self):
+        if not self.passed:
+            self.logger.critical("{} reports a failure".format(type(self)))
+            assert False
+
+
+# ==============================================================================
+
+
+class BaseEnv(uvm_env):
+    """
+    Base PyUVM test environment
+    """
+
+    def __init__(self, name, parent, scb_class):
+        super().__init__(name, parent)
+        self.scb_class = scb_class
+        self.clock_domain = parent.clock_domain
+
+    def build_phase(self):
+
+        bus_signals = [
+            "o_cpu_halt_ack",
+            "o_cpu_run_ack",
+            "o_cpu_halt_status",
+            "o_debug_mode_status",
+            "mpc_debug_halt_ack",
+            "mpc_debug_run_ack",
+            "debug_brkpt_status",
+        ]
+
+        ConfigDB().set(None, "*", "TEST_CLK_PERIOD", 1)
+        ConfigDB().set(None, "*", "TEST_ITERATIONS", 100)
+
+        # VeeR-side bus request driver
+        self.veer_bus_driver = BusDriver("veer_bus_driver", self, clock_domain=self.clock_domain)
+
+        # VeeR-side bus sequencer
+        self.veer_bus_seqr = uvm_sequencer("veer_bus_seqr", self)
+
+        # VeeR-side bus monitor
+        self.veer_bus_monitor = BusMonitor(
+            "veer_bus_monitor",
+            self,
+            clock_domain=self.clock_domain,
+            signals=[s + "_veer" for s in bus_signals],
+        )
+
+        # TMR complex output side bus monitor
+        self.out_bus_monitor = BusMonitor(
+            "out_bus_monitor",
+            self,
+            clock_domain=self.clock_domain,
+            signals=bus_signals,
+        )
+
+        # Fault status output monitor
+        self.fault_monitor = FaultMonitor(
+            "fault_monitor",
+            self,
+            clock_domain=self.clock_domain,
+            signals=[
+                cocotb.top.exec_fault_q[0],
+                cocotb.top.exec_fault_q[1],
+                cocotb.top.exec_fault_q[2],
+            ],
+        )
+
+        # Scoreboard(s)
+        self.scoreboard = None
+        if self.scb_class is not None:
+            self.scoreboard = self.scb_class("scoreboard", self)
+
+    def connect_phase(self):
+        self.veer_bus_driver.seq_item_port.connect(self.veer_bus_seqr.seq_item_export)
+        if self.scoreboard:
+            self.veer_bus_monitor.ap.connect(self.scoreboard.veer_bus_fifo.analysis_export)
+            self.out_bus_monitor.ap.connect(self.scoreboard.out_bus_fifo.analysis_export)
+            self.fault_monitor.ap.connect(self.scoreboard.fault_fifo.analysis_export)
+
+
+# ==============================================================================
+
+
+class BaseTest(common.BaseTest):
+    """
+    Base PyUVM test for the module
+    """
+
+    def __init__(self, name, parent, scb_class=None):
+        super().__init__(
+            name,
+            parent,
+            clk_name="clk",
+            rst_name="rst_l",
+            scb_class=scb_class,
+            env_class=BaseEnv,
+        )
+
+    async def initial(self):
+
+        # Initialize
+        cocotb.top.exec_fault_d[0].value = MuBiFalse
+        cocotb.top.exec_fault_d[1].value = MuBiFalse
+        cocotb.top.exec_fault_d[2].value = MuBiFalse
+
+        cocotb.top.exec_fault_clr[0].value = MuBiFalse
+        cocotb.top.exec_fault_clr[1].value = MuBiFalse
+        cocotb.top.exec_fault_clr[2].value = MuBiFalse
+
+        cocotb.top.o_cpu_halt_ack_veer.value      = [0] * 3
+        cocotb.top.o_cpu_run_ack_veer.value       = [0] * 3
+        cocotb.top.o_cpu_halt_status_veer.value   = [0] * 3
+        cocotb.top.o_debug_mode_status_veer.value = [0] * 3
+        cocotb.top.mpc_debug_halt_ack_veer.value  = [0] * 3
+        cocotb.top.mpc_debug_run_ack_veer.value   = [0] * 3
+        cocotb.top.debug_brkpt_status_veer.value  = [0] * 3
+
+        cocotb.top.i_cpu_halt_req.value     = 0
+        cocotb.top.i_cpu_run_req.value      = 0
+        cocotb.top.mpc_debug_halt_req.value = 0
+        cocotb.top.mpc_debug_run_req.value  = 0
+        cocotb.top.mpc_reset_run_req.value  = 0

1e5d717abc3a110c376f433d2a300cac40e31f22
Maciej Kurc <mkurc@antmicro.com>
2026-08-24 09:53:39 +0200

    Add misc. signals TMR voters test
    Internal-tag: [#103646]
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- verification/block/noxfile.py
+++ verification/block/noxfile.py
@@ -495,6 +495,17 @@ def tmr_ic_verify(session, blockName, testName, coverage):
 def tmr_pic_verify(session, blockName, testName, coverage):
     verify_block(session, blockName, testName, coverage)
 
+@nox.session(tags=["tests"])
+@nox.parametrize("blockName", ["tmr_if/tmr_misc"])
+@nox.parametrize(
+    "testName",
+    [
+        "test_faults",
+    ],
+)
+@nox.parametrize("coverage", coverageTypes)
+def tmr_misc_verify(session, blockName, testName, coverage):
+    verify_block(session, blockName, testName, coverage)
 
 @nox.session(reuse_venv=True)
 def lint(session: nox.Session) -> None:

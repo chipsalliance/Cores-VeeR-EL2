29f65fdde111f3f144ae0bed8bfc6894056604bf
Maciej Kurc <mkurc@antmicro.com>
2026-08-26 13:29:22 +0200

    Support overlapped AXI transactions in PyUVM AXI Agent
    Internal-tag: [#103739]
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- verification/block/common/axi_agent/axi_driver.py
+++ verification/block/common/axi_agent/axi_driver.py
@@ -23,7 +23,29 @@ class AxiDriver(uvm_driver):
         bfm_args = kwargs["bfm_args"]
         del kwargs["bfm_args"]
         self.axi_master = AxiMaster(**bfm_args)
+        self.pending_writes = Event("pending writes")
+        self.pending_reads = Event("pending reads")
         super().__init__(*args, **kwargs)
+
+    async def _write_waiter(self):
+        """
+        Waits until pending writes finish. Clears the pending writes flag
+        afterwards
+        """
+        self.raise_objection()
+        await self.axi_master.write_if.wait()
+        self.pending_writes.clear()
+        self.drop_objection()
+
+    async def _read_waiter(self):
+        """
+        Waits until pending reads finish. Clears the pending writes flag
+        afterwards
+        """
+        self.raise_objection()
+        await self.axi_master.read_if.wait()
+        self.pending_reads.clear()
+        self.drop_objection()
 
     async def run_phase(self):
 
@@ -32,24 +54,44 @@ class AxiDriver(uvm_driver):
             assert isinstance(item, AxiTransaction), type(item)
 
             if item.type == AxiTransactionType.WRITE:
-                item.resp = await self.axi_master.write(
-                    address=item.address,
-                    data=item.data,
-                    awid=item.id,
-                    burst=item.burst,
-                    size=item.size,
+                kwargs = {
+                    "address": item.address,
+                    "data": item.data,
+                    "awid": item.id,
+                    "burst": item.burst,
+                    "size": item.size,
                     # TODO: Other fields
-                )
+                }
+
+                if item.is_blocking:
+                    item.resp = await self.axi_master.write(**kwargs)
+
+                else:
+                    item.resp = None
+                    self.axi_master.init_write(**kwargs)
+                    if not self.pending_writes.is_set():
+                        self.pending_writes.set()
+                        cocotb.start_soon(self._write_waiter())
 
             elif item.type == AxiTransactionType.READ:
-                item.resp = await self.axi_master.read(
-                    address=item.address,
-                    length=item.length,
-                    arid=item.id,
-                    burst=item.burst,
-                    size=item.size,
+                kwargs = {
+                    "address": item.address,
+                    "length": item.length,
+                    "arid": item.id,
+                    "burst": item.burst,
+                    "size": item.size,
                     # TODO: Other fields
-                )
+                }
+
+                if item.is_blocking:
+                    item.resp = await self.axi_master.read(**kwargs)
+
+                else:
+                    item.resp = None
+                    self.axi_master.init_read(**kwargs)
+                    if not self.pending_reads.is_set():
+                        self.pending_reads.set()
+                        cocotb.start_soon(self._read_waiter())
 
             else:
                 assert False, item.type

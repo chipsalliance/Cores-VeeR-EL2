0fa3a8e2e58fdb7e41ad258842f02ac177d9e020
Pascal Nasahl <nasahlpa@lowrisc.org>
2026-09-14 10:49:14 +0200

    [VAL] Switch from bit to logic
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- design/lib/el2_assert.sv
+++ design/lib/el2_assert.sv
@@ -17,9 +17,9 @@
 
 // Static assertions for checks inside SV packages. If the conditions is not true, this will
 // trigger an error during elaboration.
-`define ASSERT_STATIC_IN_PACKAGE(__name, __prop)              \
-  function automatic bit assert_static_in_package_``__name(); \
-    bit unused_bit [((__prop) ? 1 : -1)];                     \
-    unused_bit = '{default: 1'b0};                            \
-    return unused_bit[0];                                     \
+`define ASSERT_STATIC_IN_PACKAGE(__name, __prop)                \
+  function automatic logic assert_static_in_package_``__name(); \
+    logic unused_bit [((__prop) ? 1 : -1)];                     \
+    unused_bit = '{default: 1'b0};                              \
+    return unused_bit[0];                                       \
   endfunction

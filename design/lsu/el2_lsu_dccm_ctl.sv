1346951f5a2f6a1ddb379f93fbcbdff5196441f9
Pascal Nasahl <nasahlpa@lowrisc.org>
2026-09-14 13:17:05 +0200

    [RTL] Fix cast width
    Signed-off-by: Pascal Nasahl <nasahlpa@lowrisc.org>

--- design/lsu/el2_lsu_dccm_ctl.sv
+++ design/lsu/el2_lsu_dccm_ctl.sv
@@ -220,8 +220,8 @@ import el2_pkg::*;
       assign dccm_dma_ecc_error   = lsu_double_ecc_error_r;
       assign dccm_dma_rtag[2:0]   = dma_mem_tag_r[2:0];
       assign dccm_dma_rdata[63:0] = ldst_dual_r ? lsu_rdata_corr_r[63:0] : {2{lsu_rdata_corr_r[31:0]}};
-      assign {lsu_ld_data_r_nc[63:32], lsu_ld_data_r[31:0]}           = lsu_rdata_r[63:0] >> 8*lsu_addr_r[1:0];
-      assign {lsu_ld_data_corr_r_nc[63:32], lsu_ld_data_corr_r[31:0]} = lsu_rdata_corr_r[63:0] >> 8*lsu_addr_r[1:0];
+      assign {lsu_ld_data_r_nc[63:32], lsu_ld_data_r[31:0]}           = 64'(lsu_rdata_r[63:0] >> 8*lsu_addr_r[1:0]);
+      assign {lsu_ld_data_corr_r_nc[63:32], lsu_ld_data_corr_r[31:0]} = 64'(lsu_rdata_corr_r[63:0] >> 8*lsu_addr_r[1:0]);
 
       assign picm_rd_data_r[63:32]   = picm_rd_data_r[31:0];
       assign dccm_rdata_r[63:0]      = {dccm_rdata_hi_r[31:0],dccm_rdata_lo_r[31:0]};
